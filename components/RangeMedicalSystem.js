a173b7f4a406edc5c19721222379d46342e9f140
range-cupp <cupp@range-medical.com>
2025-12-03 15:44:34 -0800

    Update RangeMedicalSystem.js

--- components/RangeMedicalSystem.js
+++ components/RangeMedicalSystem.js
@@ -1958,24 +1958,6 @@ const RangeMedicalSystem = () => {
       </>
       )}
     </div>
-    
-    <style>{`
-      @keyframes fadeIn {
-        from { opacity: 0; }
-        to { opacity: 1; }
-      }
-      
-      @keyframes slideInRight {
-        from { 
-          transform: translateX(100%);
-          opacity: 0;
-        }
-        to { 
-          transform: translateX(0);
-          opacity: 1;
-        }
-      }
-    `}</style>
   </div>
   );
 };

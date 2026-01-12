96edbb181153aaded10c259cd43fc1fb7d66840b
range-cupp <cupp@range-medical.com>
2026-01-11 18:45:41 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,18 +1,7 @@
-import Head from 'next/head'
-import '../styles/globals.css'
+import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
-        <link 
-          href="https://fonts.googleapis.com/css2?family=DM+Sans:wght@400;500;600;700&display=swap" 
-          rel="stylesheet" 
-        />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
 }
+
+export default MyApp;
